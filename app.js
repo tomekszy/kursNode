0beb9817cd19af6d6e2cd9df01259c49e2789883
tomekszy <red_rose@poczta.fm>
2019-03-20 12:58:23 +0100

    body parser

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const http = require('http');
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Middleware #1');
-    next();
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+        <form action="/product" method="POST">
+        <input type="text" name="title">
+        <button type="submit">Add product</button>
+        </form>
+    `);
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use((req, res, next) => {
-    console.log('Middleware #2');
-    res.send('<h1>Hello!</h1>');
+    res.send('<h1>Hello root!</h1>');
 });
 
 app.listen(3000);
