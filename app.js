fb40ba69075a570ee2ab7e5e68a36af63dd52a1c
tomekszy <red_rose@poczta.fm>
2019-03-21 10:28:37 +0100

    views

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -5,14 +7,13 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next) => {
-   res.status(404).send('<h1>Page not found</h1>');
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
-
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send(`
-        <form action="/product" method="POST">
-        <input type="text" name="title">
-        <button type="submit">Add product</button>
-        </form>
-    `);
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
-    // console.log(req.body.title);
-    res.redirect('/');
+router.post('/add-product', (req, res, next) => {
+  //   console.log(req.body);
+  console.log(req.body.title);
+  res.redirect('/');
 });
 
 module.exports = router;
