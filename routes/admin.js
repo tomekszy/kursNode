286172d4dbd0be11038a9328ef41315abd38a228
tomekszy <red_rose@poczta.fm>
2019-04-17 14:59:27 +0200

    koniec rozdziału 12

--- routes/admin.js
+++ routes/admin.js
@@ -2,23 +2,19 @@ const path = require('path');
 
 const express = require('express');
 
-const adminController = require('../controllers/admin');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
-
-// // /admin/products => GET
-// router.get('/products', adminController.getProducts);
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
-
-// router.get('/edit-product/:productId', adminController.getEditProduct);
-
-// router.post('/edit-product', adminController.postEditProduct);
-
-// router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,24 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const shopController = require('../controllers/shop');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
-
-router.get('/products', shopController.getProducts);
-
-// router.get('/products/:productId', shopController.getProduct);
-
-// router.get('/cart', shopController.getCart);
-
-// router.post('/cart', shopController.postCart);
-
-// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
-
-// router.post('/create-order', shopController.postOrder);
-
-// router.get('/orders', shopController.getOrders);
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
 
 module.exports = router;
